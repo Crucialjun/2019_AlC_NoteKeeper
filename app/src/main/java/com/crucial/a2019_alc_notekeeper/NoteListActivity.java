3034e2c9d9cd2fe184b2fa62c3c7a334393e1fc1
Crucialjun <nich.otieno@gmail.com>
2019-05-10 23:09:52 +0300

    On Click intents

--- app/src/main/java/com/crucial/a2019_alc_notekeeper/NoteListActivity.java
+++ app/src/main/java/com/crucial/a2019_alc_notekeeper/NoteListActivity.java
@@ -1,5 +1,6 @@
 package com.crucial.a2019_alc_notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
@@ -7,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -43,6 +45,15 @@ public class NoteListActivity extends AppCompatActivity {
                 new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
 
         listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this,NoteActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
