690da5a95f32fb4756d778c0a834240f4337f125
Crucialjun <nich.otieno@gmail.com>
2019-06-10 23:04:17 +0300

    Styles Addition

--- app/src/main/java/com/crucial/a2019_alc_notekeeper/MainActivity.java
+++ app/src/main/java/com/crucial/a2019_alc_notekeeper/MainActivity.java
@@ -104,7 +104,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mRecyclerItems = findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
 
-        mCoursesLayoutManager = new GridLayoutManager(this,2);
+        mCoursesLayoutManager = new GridLayoutManager(this
+                ,getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
